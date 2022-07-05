a3cb2644761cc67bfe7cc942dcf096a3df3c7418
Thiago Odilon de Almeida <74935435+teagoodilon@users.noreply.github.com>
2022-07-04 22:33:48 -0300

    Add files via upload

---
+++ A.java
@@ -0,0 +1,28 @@
+public class A{
+  private int A1;
+  private float A2;
+  
+  public int getA1(){
+    return A1;
+  }
+
+  public float getA2(){
+    return A2;
+  }
+
+  public void setA1(int A1){
+    this.A1 = A1;
+  }
+
+  public void setA2(int A2){
+    this.A1 = A2;
+  }
+
+  public void MA1(){
+    System.out.print("MA1");
+  }
+
+  public void MA2(){
+    System.out.print("MA2");
+  }
+}
---
+++ B.java
@@ -0,0 +1,28 @@
+public class B{
+  private int B1;
+  private float B2;
+  
+  public int getB1(){
+    return B1;
+  }
+
+  public float getA2(){
+    return B2;
+  }
+
+  public void setB1(int B1){
+    this.B1 = B1;
+  }
+
+  public void setB2(int B2){
+    this.B1 = B2;
+  }
+
+  public void MB1(){
+    System.out.print("MB1");
+  }
+
+  public void MB2(){
+    System.out.print("MB2");
+  }
+}
